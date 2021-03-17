981280462f7503fdb528082c3e8ecc4371d0ee73
Felix (xq) Queißner <git@mq32.de>
2021-03-17 14:30:54 +0100

    Initial release: Basic map with markers works.

---
+++ README.md
@@ -0,0 +1,21 @@
+# Zig User Map
+
+This is the second attempt on creating a cool map where you can see the (rough) locations of all ziguanas out there!
+
+## Contribution
+
+If you want to add yourself to the repo, make a PR that adds a file to the folder [`people`](people/). This file has to have the following format and should have `${your-nick}.json` as name:
+
+```json
+{
+  "nick": "xq",
+  "coordinates": [
+    48.77844,
+    9.18014
+  ],
+  "links": {
+    "Website": "https://random-projects.net",
+    "GitHub": "https://github.com/MasterQ32"
+  }
+}
+```
---
+++ src/main.zig
@@ -0,0 +1,45 @@
+const std = @import("std");
+
+pub fn main() anyerror!void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+
+    var source_files = try std.fs.cwd().openDir("people", .{ .iterate = true });
+    defer source_files.close();
+
+    var string_buffer = std.ArrayList(u8).init(&gpa.allocator);
+    defer string_buffer.deinit();
+
+    var writer = string_buffer.writer();
+
+    try writer.writeAll("[");
+
+    var first = true;
+    var iter = source_files.iterate();
+    while (try iter.next()) |entry| {
+        if (entry.kind != .File)
+            return error.InvalidFile;
+
+        var arena = std.heap.ArenaAllocator.init(&gpa.allocator);
+        defer arena.deinit();
+
+        var contents = try source_files.readFileAlloc(&arena.allocator, entry.name, 1024);
+        defer arena.allocator.free(contents);
+
+        var parser = std.json.Parser.init(&arena.allocator, false);
+        defer parser.deinit();
+
+        var tree = try parser.parse(contents);
+        defer tree.deinit();
+
+        defer first = false;
+        if (!first) {
+            try writer.writeAll(",");
+        }
+
+        try tree.root.jsonStringify(.{}, writer);
+    }
+    try writer.writeAll("]");
+
+    try std.fs.cwd().writeFile("data/users.json", string_buffer.items);
+}
