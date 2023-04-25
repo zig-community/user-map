dd3cf64d301a97c8c70507e357f335ff012640c0
Ian Johnson <ian@ianjohnson.dev>
2023-04-24 20:12:23 -0400

    Update build.zig for latest Zig master

--- build.zig
+++ build.zig
@@ -17,9 +17,9 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
-    exe.install();
+    b.installArtifact(exe);
 
-    const run_cmd = exe.run();
+    const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
     if (b.args) |args| {
         run_cmd.addArgs(args);
