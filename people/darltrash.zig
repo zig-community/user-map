a89cf5797ceafe08c2efe0fc3cc72adf4110a967
Nelson Lopez <38925212+darltrash@users.noreply.github.com>
2025-07-14 10:32:36 -0400

    Create darltrash.zig

---
+++ people/darltrash.zig
@@ -0,0 +1,12 @@
+{
+  "nick": "darltrash",
+  "coordinates": [
+    ﻿-35.4269444, 
+    -71.6655555
+  ],
+  "links": {
+    "Organization": "https://starlightnet.work",
+    "Github": "https://github.com/darltrash",
+    "Website": "https://n.cyrneko.eu"
+  }
+}
